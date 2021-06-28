b09444bc6bb1a70a9469ef71f20d0fab81db74d4
Waliston E. Santos <82295321+bkt-tonton@users.noreply.github.com>
2021-06-28 10:14:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,107 @@
+/*abre e fecha o menu ao clicar */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu esconder todo ele */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function(){
+    nav.classList.remove('show')
+  })
+}
+
+/* SOMBRA DO SCROLL */
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  if(window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(`#home .text, #home .image`,{ interval: 100})
+scrollReveal.reveal(`#about .image, #about .text`,{ interval: 100})
+scrollReveal.reveal(`#services header, #services .card`,{ interval: 100})
+scrollReveal.reveal(`#testimonials header, #estimonials .testimonials`,{ interval: 100})
+scrollReveal.reveal(`#contact .text, #contact .link`,{ interval: 100})
+scrollReveal.reveal(`#footer .brand, footer .social`,{ interval: 100})
+
+/* Back to top */
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* Menu Ativo */
+const sections = document.querySelectorAll('main section[id]')
+function activeMenuAtCurrentSection() {
+
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8 ) * 4
+
+  for (const section of sections ) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if(checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.remove('active')
+    }
+      
+  }
+}
+/* when Scroll */ 
+window.addEventListener('scroll', function() {
+  changeHeaderWhenScroll()
+  backToTop()
+  activeMenuAtCurrentSection()
+})
